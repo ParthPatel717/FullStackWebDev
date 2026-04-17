6b04331a77413d561e4dd46b8122b46197249fc8
Parth patel <116782706+ParthPatel717@users.noreply.github.com>
2026-04-17 15:31:06 +0530

    Create exercise1_7.jsx

---
+++ part1/exercise1_7.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good+neutral+bad, average =0
+
+  const updateGood = () => {
+    const updated = good + 1;
+    setGood(updated);
+  };
+
+  const updateNeutral = () => {
+    const updated = neutral + 1;
+    setNeutral(updated);
+  };
+  const updateBad = () => {
+    const updated = bad + 1;
+    setBad(updated);
+  };
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => updateGood()}>Good</button>
+      <button onClick={() => updateNeutral()}>Neutral</button>
+      <button onClick={() => updateBad()}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>Total {good+bad+neutral}</p>
+      <p>Average {(good-bad)/total}</p>
+    </div>
+  );
+};
+export default App;
