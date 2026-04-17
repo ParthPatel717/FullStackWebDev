bc5aa22f73d0b30fca74b4ac4fdd6122a3404464
Parth patel <116782706+ParthPatel717@users.noreply.github.com>
2026-04-17 22:51:27 +0530

    Create exercise1_8.jsx

---
+++ part1/exercise1_8.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+const Statistics = ({good,bad,neutral,total}) => {
+  return(
+  <div>
+    
+   <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+       <p>Total {total}</p>
+        <p>Average {total===0?0:(good-bad)/total}</p>
+      <p>Positive {total===0?0:(good/total)*100}%</p>
+  </div>
+  )
+}
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good+neutral+bad, average =0
+
+  const updateGood = () => {
+    const updated = good + 1;
+    setGood(updated);
+  };
+
+  const updateNeutral = () => {
+    const updated = neutral + 1;
+    setNeutral(updated);
+  };
+  const updateBad = () => {
+    const updated = bad + 1;
+    setBad(updated);
+  };
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => updateGood()}>Good</button>
+      <button onClick={() => updateNeutral()}>Neutral</button>
+      <button onClick={() => updateBad()}>Bad</button>
+   
+     
+     
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+    </div>
+  );
+};
+export default App;
