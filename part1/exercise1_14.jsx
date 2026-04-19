d351f37f0838ed1c307b53d396e6e29eb784daa4
Parth patel <116782706+ParthPatel717@users.noreply.github.com>
2026-04-19 23:23:02 +0530

    Create exercise1_14.jsx

---
+++ part1/exercise1_14.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+     const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0,0])
+  const updateVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] +=1
+    setVotes(newVotes)
+  }
+  const newQuote = () => {
+    const randomnum = Math.floor(Math.random()*8)
+    setSelected(randomnum);
+    console.log(randomnum)
+  }
+const maxVotes =  Math.max(...votes)
+const index = votes.indexOf(maxVotes)
+  return(
+    <div>
+       <p> {anecdotes[selected]}</p>
+        <button onClick={() => newQuote()}>Update</button>
+        <button onClick={() => updateVote()}>Vote</button>
+        
+        <h1>Most Voted Quote</h1>
+        <p>{anecdotes[index]}</p>
+        <p>Votes: {maxVotes}</p>
+    </div>
+  )
+};
+export default App;
