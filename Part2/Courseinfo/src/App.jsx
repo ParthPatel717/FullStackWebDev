6d33aab692684e4cf13bead034173c22c3cc03bc
parth7179 <parthpatel71791@gmail.com>
2026-06-19 14:28:38 +0530

    exercise 2.5

--- Part2/Courseinfo/src/App.jsx
+++ Part2/Courseinfo/src/App.jsx
@@ -1,34 +1,4 @@
-const CourseDetails = ({name, parts}) =>{
-    const total_exercise = parts.reduce(function(sum,part){
-    return sum + part.exercises;
-  },0)
-  return(
-    <div>
-      <h3>{name}</h3>
-      <div> 
-        {parts.map( part=> <p key = {part.id}>{part.name} {part.exercises} </p> )}
-      </div>
-      <div> Total of {total_exercise} exercises </div>
-    </div>
-    
-  )
-}
-const Course = ({courses})=>{
-    return(
-        <div>
-          <h1>Web development curriculam</h1>
-            <div>
-              {
-                courses.map(core => {
-                  return(
-                    <CourseDetails key={core.id} name={core.name} parts={core.parts}/>
-                  )
-                })
-              }
-            </div>
-        </div>
-    )
-}
+import Course from "./components/Course"
 
 const App = () => {
    const courses = [
